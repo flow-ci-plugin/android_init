7e83d3b334388aa80fb69a39b2d3a6421c1b429e
yang.guo <gy@fir.im>
2017-04-19 12:53:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,31 +18,11 @@ echo "\$FLOW_EVENT_NUMBER = $FLOW_EVENT_NUMBER"
 echo "\$FLOW_PHP_OLD_VERSION = $FLOW_PHP_OLD_VERSION"
 
 echo '=================================================='
-# ************************************************************
-#
-# This step will init your project
-#
-#   Variables used:
-#     $FLOW_VERSION
-#     $FLOW_PROJECT_ANDROID_SDK
-#     $FLOW_PROJECT_NAME
-#     $FLOW_PROJECT_ANDROID_CPU
-#
-#   Outputs:
-#     $FLOW_JAVA_VERSION
-#     $FLOW_ANDROID_SDK_VERSION
-#     $FLOW_ANDROID_CPU_VERSION
-#
-# ************************************************************
 
 echo "\$FLOW_ALIAS_NAME = $FLOW_ALIAS_NAME"
 echo "\$FLOW_ALIAS_PASS = $FLOW_ALIAS_PASS"
 echo "\$FLOW_KEYSTORE_PASS = $FLOW_KEYSTORE_PASS"
 echo "\$FLOW_CERTIFICATE_FILE = $FLOW_CERTIFICATE_FILE"
-
-flow_cmd "echo $FLOW_PROJECT_ANDROID_SDK" --echo
-flow_cmd "echo $FLOW_PROJECT_ANDROID_CPU" --echo
-flow_cmd "echo $ANDROID_NDK_HOME" --echo
 
 source $HOME/.nvm/nvm.sh
 source $HOME/.rvm/scripts/rvm
@@ -52,8 +32,3 @@ flow_cmd "chjava $FLOW_VERSION" --echo
 
 flow_cmd "java -Xmx32m -version" --echo
 flow_cmd "javac -J-Xmx32m -version" --echo
-
-export FLOW_JAVA_VERSION="$FLOW_VERSION"
-export FLOW_ANDROID_SDK_VERSION="$FLOW_PROJECT_ANDROID_SDK"
-export FLOW_ANDROID_CPU_VERSION="$FLOW_PROJECT_ANDROID_CPU"
-
